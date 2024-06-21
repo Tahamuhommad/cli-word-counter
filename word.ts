9a692f13e06ab789d01b0cd4cd3d64650d7e8f04
Tahamuhommad <muhommadtaha52@gmail.com>
2024-06-21 17:39:03 +0500

    first commit

---
+++ word.ts
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer'
+
+const answer:{
+    sentence:string
+} = await inquirer.prompt([{
+    name:'sentence',
+    type:'input',
+    message:'Enter your sentence to count the word:'
+}])
+
+const words = answer.sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`Your sentence word count is ${words.length}`);
